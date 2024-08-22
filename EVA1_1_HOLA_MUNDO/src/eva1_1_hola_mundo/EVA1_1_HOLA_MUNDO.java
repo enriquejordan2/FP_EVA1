d46bb3af20fe517e0469324c3a9824531ef9f55b
enriquejordan2 <L24550342@chihuahua2.tecnm.mx>
2024-08-22 16:55:51 -0600

    Add files via upload

---
+++ EVA1_1_HOLA_MUNDO/src/eva1_1_hola_mundo/EVA1_1_HOLA_MUNDO.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_1_hola_mundo;
+
+/**
+ *
+ * @author Jose Enrique
+ */
+public class EVA1_1_HOLA_MUNDO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Mostrar un Saludo--->Documentar
+        System.out.println("Hola Mundo!!");
+    }
+    
+}
